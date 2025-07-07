0c9bea5b0578359eb109387dca34343706b484fe
Jay-R-Goyani <jaygoyani2004@gmail.com>
2025-07-08 00:11:55 +0530

    error e2 solved

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const cors = require('cors');
-const user = require("../routes/user");
-const connectDb = require("../config/dbconnection");
-const blogRoutes = require('../routes/blogRoutes');
-const crud = require('../routes/crud')
-const college = require('../routes/college')
-const { cloudinaryConnect } = require("../config/cloudinary")
+const user = require("./routes/user");
+const connectDb = require("./config/dbconnection");
+const blogRoutes = require('./routes/blogRoutes');
+const crud = require('./routes/crud')
+const college = require('./routes/college')
+const { cloudinaryConnect } = require("./config/cloudinary")
 require("dotenv").config();
-const eventRoutes = require("../routes/eventRoutes")
-const homepageRoutes = require("../routes/homepageRoutes")
-const collegeRep = require('../routes/collegeRep')
-const adminroutes = require("../routes/adminroutes")
-const adminAuthRoutes = require("../routes/adminAuth");
+const eventRoutes = require("./routes/eventRoutes")
+const homepageRoutes = require("./routes/homepageRoutes")
+const collegeRep = require('./routes/collegeRep')
+const adminroutes = require("./routes/adminroutes")
+const adminAuthRoutes = require("./routes/adminAuth");
 const app = express();
 
 const corsOptions = {
@@ -28,7 +28,9 @@ app.use(express.urlencoded({ extended: false }))
 
 
 const PORT = process.env.PORT || 3000;
-
+app.get('/', (req, res) => {
+  res.send('Express backend working on Vercel');
+});
 app.use("/api/auth", user);
 app.use("/api/blog", blogRoutes);
 app.use("/api/users", crud);
