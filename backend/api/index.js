f13b376cfa777f099934d034a0e81d9f4b5cd304
Jay-R-Goyani <jaygoyani2004@gmail.com>
2025-07-08 11:50:25 +0530

    update u7

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require("dotenv").config();
-const connectDb = require("./config/dbconnection");
-const serverless = require('serverless-http');
+const connectDb = require("../config/dbconnection");
 
 
 const app = express();
@@ -18,15 +17,15 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const user = require("./routes/user");
-const blogRoutes = require('./routes/blogRoutes');
-const crud = require('./routes/crud');
-const college = require('./routes/college');
-const eventRoutes = require("./routes/eventRoutes");
-const homepageRoutes = require("./routes/homepageRoutes");
-const collegeRep = require('./routes/collegeRep');
-const adminroutes = require("./routes/adminroutes");
-const adminAuthRoutes = require("./routes/adminAuth");
+const user = require("../routes/user");
+const blogRoutes = require('../routes/blogRoutes');
+const crud = require('../routes/crud');
+const college = require('../routes/college');
+const eventRoutes = require("../routes/eventRoutes");
+const homepageRoutes = require("../routes/homepageRoutes");
+const collegeRep = require('../routes/collegeRep');
+const adminroutes = require("../routes/adminroutes");
+const adminAuthRoutes = require("../routes/adminAuth");
 
 app.get('/', (req, res) => {
     res.send("Backend is live!");
@@ -44,4 +43,4 @@ app.use("/api/admin-auth", adminAuthRoutes);
 
 connectDb();
 
-module.exports = serverless(app);
+module.exports = app;
