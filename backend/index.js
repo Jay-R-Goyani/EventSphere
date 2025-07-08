7b7d5dc68ec1de071a8aa443655d2a609ec70150
Jay-R-Goyani <jaygoyani2004@gmail.com>
2025-07-08 11:09:22 +0530

    update u4

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const app = express();
 const corsOptions = {
-    origin: "*",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
